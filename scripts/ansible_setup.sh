8c0aea2c33c1eb60890cccbf038c63afde424c8e
innyso <innyso@gmail.com>
2014-02-07 02:03:33 +1100

    initial commit

---
+++ scripts/ansible_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+apt-get --quiet --assume-yes install python-dev
+apt-get --quiet --assume-yes install python-pip
+yes | pip install paramiko
+yes | pip install pyYAML
+yes | pip install jinja2
+yes | pip install ansible
