6537c41db898cb3f4af4ef5d6ba0ee19eb9bdfe8
Inny <inny@Innys-MacBook-Air-2.local>
2014-02-26 01:30:35 +0000

    working in progress for oh-my-zsh ansible script

--- scripts/ansible_setup.sh
+++ scripts/ansible_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update
+apt-get --quiet --assume-yes install sshpass
 apt-get --quiet --assume-yes install python-dev
 apt-get --quiet --assume-yes install python-pip
 yes | pip install paramiko
---
+++ scripts/key_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ssh-keygen -q -t rsa -f /home/vagrant/.ssh/id_rsa -N ""
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+touch /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
