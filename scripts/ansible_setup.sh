184f5cb02d7173f69264ed022a737a2ecf438487
Inny <inny@Innys-MacBook-Air-2.local>
2014-03-01 02:42:09 +0000

    add vundle and vim settings

--- scripts/ansible_setup.sh
+++ scripts/ansible_setup.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get --quiet --assume-yes install sshpass
 apt-get --quiet --assume-yes install python-dev
 apt-get --quiet --assume-yes install python-pip
+apt-get --quiet --assume-yes install python-keyczar
 yes | pip install paramiko
 yes | pip install pyYAML
 yes | pip install jinja2
